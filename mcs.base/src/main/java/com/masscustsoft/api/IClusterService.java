f6f4163bfe4cb44b6b9c9ea33339102b9d819629
Jason Song <jasongcs@gmail.com>
2017-03-04 08:44:15 -0500

    API and ryhno

--- mcs.base/src/main/java/com/masscustsoft/api/IClusterService.java
+++
@@ -1,8 +0,0 @@
-package com.masscustsoft.api;
-
-public interface IClusterService {
-	public String myClusterId();
-	public String myNodeId();
-	public String myBindIp();
-	public String myBindPort();
-}
